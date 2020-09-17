528baec85b97dea0066e24b487c3d29bcf75484a
Ashish <aashishpande944@gmail.com>
2020-09-17 17:10:14 +0530

    Added step to create user

--- app/concepts/user/operation/create.rb
+++ app/concepts/user/operation/create.rb
@@ -4,6 +4,7 @@ module User::Operation
     fail :log_error_for_email
     step :check_password
     fail :log_error_invalid_password
+    step :create_user
 
     def check_email?(ctx, **)
       ctx[:params][:email].present?  
@@ -18,7 +19,11 @@ module User::Operation
     end
 
     def log_error_invalid_password(ctx, **)
-      ctx[:password] = 'The confirm password is incorrect'
+      ctx[:password] = 'confirm password is incorrect'
+    end
+
+    def create_user(ctx, **)
+      User.create(ctx[:params])
     end
   end
 end

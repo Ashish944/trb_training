c77b55d48c64360b57c9727d568ad4a83300c089
Ashish <aashishpande944@gmail.com>
2020-09-17 10:24:50 +0530

    Added fields role and password into User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_121655) do
+ActiveRecord::Schema.define(version: 2020_09_17_045058) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
+    t.string "password"
   end
 
 end

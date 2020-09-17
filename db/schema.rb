5137e0ab8faccc4c6240476cedf434a92b6cf031
Ashish <aashishpande944@gmail.com>
2020-09-17 17:19:24 +0530

    Add field password confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_045058) do
+ActiveRecord::Schema.define(version: 2020_09_17_114724) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_045058) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
     t.string "password"
+    t.string "password_confirmation"
   end
 
 end

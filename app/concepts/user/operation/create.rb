8a26939caa137d3d4f79ebd6ca1a44174dd26461
Ashish <aashishpande944@gmail.com>
2020-09-18 15:00:17 +0530

    Updated user operation create to check role param

--- app/concepts/user/operation/create.rb
+++ app/concepts/user/operation/create.rb
@@ -1,10 +1,16 @@
 module User::Operation
   class Create < Trailblazer::Operation
-    step :check_email?
+    step :check_email?, fail_fast: true
     fail :log_error_for_email
     step :check_password
     fail :log_error_invalid_password
-    step :create_user
+    # pass :check_count
+    step :role_present?, Output(:failure) => Id(:failure_role)
+    step :success_role
+    fail :failure_role
+    # step :create_user#, pass_fast: true
+
+    pass ->(ctx, **) { ctx[:message] = 'Lambda Operation' }
 
     def check_email?(ctx, **)
       ctx[:params][:email].present?  
@@ -24,6 +30,27 @@ module User::Operation
 
     def create_user(ctx, **)
       User.create(ctx[:params])
+    end
+
+    def check_count(ctx, **)
+      ctx[:response] = "User count - #{Use.count}"
+      true
+    end
+
+    def role_present?(ctx, params:, **)
+      params[:role].present?
+    end
+
+    def success_role(ctx, **)
+      ctx[:role_success] = {
+        message: 'Role Present'
+      }
+    end
+
+    def success_role(ctx, **)
+      ctx[:role_success] = {
+        message: 'Role Not Present'
+      }
     end
   end
 end
